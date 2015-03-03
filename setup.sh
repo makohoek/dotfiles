8661cb32c5e7184fb9fdb61920cabb6f815b796b
Mattijs Korpershoek <mattijsx.korpershoek@intel.com>
2015-03-03 10:09:10 +0100

    Setup script for new workstations
    This is to ease my migration on other computers.
    
    Signed-off-by: Mattijs Korpershoek <mattijsx.korpershoek@intel.com>

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.bashrc ~/
+ln -s ~/dotfiles/.bash_aliases ~/
+ln -s ~/dotfiles/.bash_git_prompt ~/
+ln -s ~/dotfiles/.vimrc ~/
+ln -s ~/dotfiles/.vim ~/
+ln -s ~/dotfiles/.tmux.conf ~/
+ln -s ~/dotfiles/.gitconfig ~/
+ln -s ~/dotfiles/.githelpers ~/
+ln -s ~/dotfiles/.gvimrc ~/
+ln -s ~/dotfiles/.gvimrc ~/
+ln -s ~/dotfiles/.ycm_extra_conf.py ~/
