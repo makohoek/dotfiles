70b38b6ff075f31a753a20f89480e0388f04bd9a
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2015-07-02 07:27:14 +0200

    Nvim: fugitive commit script to take arguments
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- neovim_helpers/run_fugitive_commit.py
+++ neovim_helpers/run_fugitive_commit.py
@@ -4,10 +4,12 @@ from neovim import attach
 import os
 import sys
 
-# Create a python API session attached to unix domain socket created above:
-nvim = attach('socket', path='/tmp/nvim')
-current_directory = os.path.expandvars('$PWD') + '/'
-nvim.command('cd ' + current_directory)
-# initialize fugitive
-nvim.command('call fugitive#detect(\''+current_directory+'\')')
-nvim.command('Gcommit -s')
+if __name__ == '__main__':
+    # Create a python API session attached to unix domain socket created above:
+    nvim = attach('socket', path='/tmp/nvim')
+    current_directory = os.path.expandvars('$PWD') + '/'
+    nvim.command('cd ' + current_directory)
+    # initialize fugitive
+    nvim.command('call fugitive#detect(\''+current_directory+'\')')
+    commit_args=' '.join(sys.argv)
+    nvim.command('Gcommit '+commit_args)
