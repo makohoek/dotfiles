941305530e1e514f03aeae356a8a5d4c6053b241
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2015-09-20 12:51:36 +0200

    emacs: new init.el with colorscheme preferences
    I am willing to give emacs another try, since some some stuff, such as
    tramp seem really awesome to me.
    
    This patch pushes my first version of init.el with some theme
    customisation.
    It is quite ugly but i don't know how to make this better for the moment.

---
+++ .emacs.d/init.el
@@ -0,0 +1,32 @@
+;; Other packages source list for package-list
+(setq package-archives
+ '(("gnu" . "http://elpa.gnu.org/packages/")
+  ("marmalade" . "http://marmalade-repo.org/packages/")
+  ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
+
+(require 'whitespace)
+
+;; Theme specific stuff
+;; use cursorline as in vim
+(global-hl-line-mode)
+;; colorscheme
+(load-theme 'base16-eighties-dark)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3539b3cc5cbba41609117830a79f71309a89782f23c740d4a5b569935f9b7726" default)))
+ '(whitespace-style
+   (quote
+    (face tabs trailing space-before-tab empty space-after-tab tab-mark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
