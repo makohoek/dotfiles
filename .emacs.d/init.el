0c8cbedfce416a59328f2b3d43119e6ccda0e797
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2015-09-20 14:43:30 +0200

    emacs: enable ido mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,6 +16,9 @@
 ;; remove scroll bar
 (scroll-bar-mode -1)
 
+;; enable ido mode
+(ido-mode 1)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
