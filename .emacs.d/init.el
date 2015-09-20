805a5e1d0f67fa2496f9397fc7d545393e7f0afe
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2015-09-20 14:39:18 +0200

    emacs: do not show ugly scroll bar
    scroll bar depends on window manager, which can be pretty ugly.
    Remove the scroll bar entirely.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,6 +13,9 @@
 ;; colorscheme
 (load-theme 'base16-eighties-dark)
 
+;; remove scroll bar
+(scroll-bar-mode -1)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
