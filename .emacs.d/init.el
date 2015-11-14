c20b79da71a35f6a232028923aa2f723a54b1989
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2015-11-14 15:50:43 +0100

    emacs: remove marmelade package repo
    I don't use this and want to keep my config clean.
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,6 @@
 ;; Other packages source list for package-list
 (setq package-archives
  '(("gnu" . "http://elpa.gnu.org/packages/")
-  ("marmalade" . "http://marmalade-repo.org/packages/")
   ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
