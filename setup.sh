21ad54d0e23689934137a5b79a2cc8ab71df436e
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2018-06-05 15:07:53 -0700

    setup.sh: use new script for git bare repo
    Cheers to https://bitbucket.org/durdn/ for his blog post:
    https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/.bashrc ~/
-ln -s ~/dotfiles/.bash_aliases ~/
-ln -s ~/dotfiles/.bash_git_prompt ~/
-ln -s ~/dotfiles/.vimrc ~/
-ln -s ~/dotfiles/.vim ~/
-ln -s ~/dotfiles/.tmux.conf ~/
-ln -s ~/dotfiles/.gitconfig ~/
-ln -s ~/dotfiles/.githelpers ~/
-ln -s ~/dotfiles/.gvimrc ~/
-ln -s ~/dotfiles/.gvimrc ~/
-ln -s ~/dotfiles/.ycm_extra_conf.py ~/
-ln -s ~/dotfiles/.Xmodmap ~/
-ln -s ~/dotfiles/.spacemacs ~/
-ln -s ~/dotfiles/.xprofile ~/
-ln -s ~/dotfiles/.i3blocks.conf ~/
-ln -s ~/dotfiles/.tmux-colors.conf ~/
-ln -s ~/dotfiles/.tmux-osx.conf ~/
-ln -s ~/dotfiles/.tmux-linux.conf ~/
+# stolen from
+# https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
 
-mkdir ~/.gnupg
-ln -s ~/dotfiles/.gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+# great idea!
+
+git clone --bare https://github.com/makohoek/dotfiles $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config submodule update --init --recursive
