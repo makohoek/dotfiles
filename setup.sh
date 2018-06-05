9a1770fdaee701e71ecf8de7d8be1ccca06efa45
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2018-06-05 15:38:28 -0700

    setup: always back up existing dotfiles
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- setup.sh
+++ setup.sh
@@ -10,12 +10,8 @@ function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 mkdir -p .config-backup
-config checkout
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
+echo "Backing up pre-existing dot files.";
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+
 config checkout
 config submodule update --init --recursive
