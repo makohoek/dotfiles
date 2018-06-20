3f9d0a1162e70053e3200aedaef83c59698d0f72
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2018-06-20 10:09:32 -0700

    spacemacs: custom: remove background settings
    Want to be able to switch themes without custom interfering my themes.
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- .config/spacemacs/custom.el
+++ .config/spacemacs/custom.el
@@ -6,9 +6,3 @@
  '(package-selected-packages
    (quote
     (elisp-format stickyfunc-enhance srefactor zenburn-theme yapfify yaml-mode xterm-color xcscope xclip which-key wgrep web-mode web-beautify vimrc-mode use-package toc-org tagedit spaceline spacegray-theme solarized-theme smex smeargle slim-mode shell-pop seoul256-theme scss-mode sass-mode reveal-in-osx-finder realgud pyvenv pytest pyenv-mode py-isort pug-mode popwin pip-requirements persp-mode pcre2el pbcopy ox-reveal osx-trash osx-dictionary orgit org-projectile org-present org-pomodoro org-mime org-jira org-download org-bullets multi-term mu4e-maildirs-extension mu4e-alert mmm-mode markdown-toc magit-gitflow macrostep livid-mode live-py-mode less-css-mode launchctl json-mode js2-refactor js-doc ivy-hydra insert-shebang hy-mode htmlize hl-todo helm-make golden-ratio go-guru go-eldoc gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md fuzzy flyspell-correct-ivy flx fish-mode fill-column-indicator eyebrowse exec-path-from-shell evil-visualstar evil-magit evil-escape eshell-z eshell-prompt-extras esh-help emmet-mode elisp-slime-nav dtrt-indent disaster dactyl-mode cython-mode counsel-projectile copy-as-format company-web company-tern company-statistics company-shell company-go company-c-headers company-anaconda color-theme-sanityinc-tomorrow coffee-mode cmake-mode clang-format bind-map base16-theme auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile ag ace-window ac-ispell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:foreground "#DCDCCC" :background "#3F3F3F")))))
