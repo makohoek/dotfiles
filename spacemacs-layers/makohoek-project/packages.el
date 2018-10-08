bfa9b87968d76db1f0b020b1c5cc8fc33f9b62e6
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2018-10-08 16:53:35 -0700

    spacemacs: project: fix project path
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- spacemacs-layers/makohoek-project/packages.el
+++ spacemacs-layers/makohoek-project/packages.el
@@ -39,7 +39,7 @@
 (setq private-android-allowed-targets '("aosp_dragon" "aosp_shamu"))
 
 ;; Load private projects, if they exist
-(setq private-projects "~/.dotfiles-private/spacemacs-layers//makohoek-dev/projects.el")
+(setq private-projects "~/.dotfiles-private/spacemacs-layers/makohoek-project/projects.el")
 (load private-projects 't)
 
 (defun makohoek-project-make-android-prefix (target)
