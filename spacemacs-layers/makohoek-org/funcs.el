da7dc15dd4def19514edca415258b31645af682b
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2018-11-02 14:12:51 -0700

    spacemacs: makohoek-org: guard func definition based on org usage
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- spacemacs-layers/makohoek-org/funcs.el
+++ spacemacs-layers/makohoek-org/funcs.el
@@ -9,32 +9,34 @@
 ;;
 ;;; License: GPLv3
 
-;; org-sync + makefile from jgoerzen (thanks !)
-;; https://github.com/jgoerzen/public-snippets/blob/master/emacs/org-tools/emacs-config.org
-(defun makohoek-org/org-sync-sentinel (_process retcode)
-  "Handle output from the org-sync"
-  (if (equal retcode "finished\n")
-      (progn (org-revert-all-org-buffers)
-             (org-id-update-id-locations)
-             (org-save-all-org-buffers)
-             (switch-to-buffer "*Org Sync Output*")
-             (make-process
-              :name "org-sync"
-              :buffer "*Org Sync Output*"
-              :command (cl-list* "make" (list "-C" "~/org" "push"))
-              ))))
 
-(defun makohoek-org/org-sync ()
-  "Sync org."
-  (interactive)
-  (org-save-all-org-buffers)
-  (switch-to-buffer "*Org Sync Output*")
-  (erase-buffer)
-  (make-process
-   :name "org-sync"
-   :buffer "*Org Sync Output*"
-   :command (cl-list* "make" (list "-C" "~/org"))
-   :sentinel 'makohoek-org/org-sync-sentinel))
+(when (configuration-layer/package-used-p 'org)
+  ;; org-sync + makefile from jgoerzen (thanks !)
+  ;; https://github.com/jgoerzen/public-snippets/blob/master/emacs/org-tools/emacs-config.org
+  (defun makohoek-org/org-sync-sentinel (_process retcode)
+    "Handle output from the org-sync"
+    (if (equal retcode "finished\n")
+        (progn (org-revert-all-org-buffers)
+               (org-id-update-id-locations)
+               (org-save-all-org-buffers)
+               (switch-to-buffer "*Org Sync Output*")
+               (make-process
+                :name "org-sync"
+                :buffer "*Org Sync Output*"
+                :command (cl-list* "make" (list "-C" "~/org" "push"))
+                ))))
+
+  (defun makohoek-org/org-sync ()
+    "Sync org."
+    (interactive)
+    (org-save-all-org-buffers)
+    (switch-to-buffer "*Org Sync Output*")
+    (erase-buffer)
+    (make-process
+     :name "org-sync"
+     :buffer "*Org Sync Output*"
+     :command (cl-list* "make" (list "-C" "~/org"))
+     :sentinel 'makohoek-org/org-sync-sentinel)))
 
 (defun makohoek-org/ews-work-calendar ()
   (interactive)
