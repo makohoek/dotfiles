b99f5587e2868383df3492ba101c25325fdf8481
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2019-04-04 17:17:58 -0700

    spacemacs: layers: dev: fix signed-off-by for recent magit
    Magit-popup has been refactored, which broke some things.
    
    References:
        https://github.com/JulienMasson/jm-config/commit/f3d84167d0791f15dfdf0920ae165024aa5c5db6
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- spacemacs-layers/makohoek-dev/packages.el
+++ spacemacs-layers/makohoek-dev/packages.el
@@ -59,8 +59,8 @@
       ;; don't display --graph on magit-log
       (setq magit-log-arguments '("-n30" "--decorate"))
       ;; signed-off by default
-      (setq magit-commit-arguments (quote ("--signoff")))
-      (setq magit-revert-arguments (quote ("--signoff")))
+      (setq transient-values '((magit-commit "--signoff")
+                               (magit-revert "--signoff")))
       ;; better rebase
       (setq magit-rebase-arguments (quote ("--autosquash" "--autostash")))
       ;; title must be no longer than 50
