cd1bbee18b1096886842a470d1456aa7ae99d6bc
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2019-09-27 12:55:02 -0700

    spacemacs: projectile: add uboot env and fip.bin output config
    This way, we do everything in one compilation command
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- spacemacs-layers/makohoek-project/funcs.el
+++ spacemacs-layers/makohoek-project/funcs.el
@@ -3,8 +3,25 @@
   (setq makohoek-project/android/allowed-targets '("aosp_dragon" "aosp_shamu"))
   (setq makohoek-project/uboot/allowed-targets '("aosp_dragon" "aosp_shamu"))
 
+  (setq makohoek-project/uboot/env-out-for-target
+        '(("aosp_dragon" . "~/aosp/device/google/dragon/u-boot-env")
+          ("aosp_shamu" . "~/aosp/device/google/shamu/u-boot-env")))
+
+  (setq makohoek-project/uboot/fip-out-for-target
+        '(("aosp_dragon" . "~/aosp/device/google/dragon/fip.bin")
+          ("aosp_shamu" . "~/aosp/device/google/shamu/fip.bin")))
+
   ;; override project definitions, if they are present
   (load "~/.dotfiles-private/spacemacs-layers/makohoek-project/projects.el" 't)
+
+  (defun makohoek-project/projectile-clear-compilation-cmd-cache ()
+    "Clears the compilation command cache
+This is useful when switching between different lunch targets."
+    ;; project is in our database: we don't want to use the "cached compilation cmd"
+    ;; FIXME: should remove only the key/value for this project, not all
+    (interactive)
+    (clrhash projectile-compilation-cmd-map)
+    (clrhash projectile-test-cmd-map))
 
   (defun makohoek-project/android/compile-prefix (lunch-target)
     "Returns a string representing the begin of an Android compilation command"
@@ -28,15 +45,6 @@
             command
             (makohoek-project/android/compile-suffix)))
 
-  (defun makohoek-project/clear-compilation-cmd-cache ()
-    "Clears the compilation command cache
-This is useful when switching between different lunch targets."
-    ;; project is in our database: we don't want to use the "cached compilation cmd"
-    ;; FIXME: should remove only the key/value for this project, not all
-    (interactive)
-    (clrhash projectile-compilation-cmd-map)
-    (clrhash projectile-test-cmd-map))
-
   (defun makohoek-project/android/compile-kernel ()
     "Returns a String representing how to compile kernel in Android"
     (makohoek-project/android/compile-command "make bootimage"))
@@ -53,15 +61,24 @@ This is useful when switching between different lunch targets."
      "make " config_name "_defconfig" " && "
      "ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- " ))
 
-  (defun makohoek-project/uboot/compile-suffix ()
+  (defun makohoek-project/uboot/compile-suffix (env-out-file fip-image)
     "Returns a string representing the end of an Uboot compilation command"
-    "'")
+    (concat
+     " && ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- "
+     "scripts/get_default_envs.sh " " > "
+     env-out-file
+     " && " "fiptool update "
+     fip-image
+     " --nt-fw u-boot.bin"
+     "'"))
 
   (defun makohoek-project/uboot/compile-command (command)
     (setq target (completing-read "Target: " makohoek-project/uboot/allowed-targets))
+    (setq env-out-file (cdr (assoc target makohoek-project/uboot/env-out-for-target)))
+    (setq fip-out-file (cdr (assoc target makohoek-project/uboot/fip-out-for-target)))
     (concat (makohoek-project/uboot/compile-prefix target)
             command
-            (makohoek-project/uboot/compile-suffix)))
+            (makohoek-project/uboot/compile-suffix env-out-file fip-out-file)))
 
   (defun makohoek-project/uboot/compile ()
     "Returns a String representing how to compile Uboot"
