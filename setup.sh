a4f8707a5976cf99c634ad427e598978f739c67f
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2019-12-14 14:50:19 -0800

    stow: add setup.sh script
    Borrowed from:
    https://dev.to/writingcode/how-i-manage-my-dotfiles-using-gnu-stow-4l59
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# shamelessy stolen from:
+# https://dev.to/writingcode/how-i-manage-my-dotfiles-using-gnu-stow-4l59
+
+# make sure we have pulled in and updated any submodules
+git submodule init
+git submodule update
+
+# what directories should be installable by all users including the root user
+base=(
+    bash
+)
+
+# folders that should, or only need to be installed for a local user
+useronly=(
+    git
+)
+
+# run the stow command for the passed in directory ($2) in location $1
+stowit() {
+    usr=$1
+    app=$2
+    # -v verbose
+    # -R recursive
+    # -t target
+    stow -v -R -t ${usr} ${app}
+}
+
+echo ""
+echo "Stowing apps for user: ${whoami}"
+
+# install apps available to local users and root
+for app in ${base[@]}; do
+    stowit "${HOME}" $app 
+done
+
+# install only user space folders
+for app in ${useronly[@]}; do
+    if [[! "$(whoami)" = *"root"*]]; then
+        stowit "${HOME}" $app 
+    fi
+done
+
+echo ""
+echo "##### ALL DONE"
