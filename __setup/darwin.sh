fb740ed38430380dbdf548b820e7886e7afdd98b
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2019-12-14 16:02:31 -0800

    stow: setup: install oh my zsh as well on darwin
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- __setup/darwin.sh
+++ __setup/darwin.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-# clone oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# This should be ran PRIOR to running stow
 
+# install brew
+command -v brew >/dev/null 2>&1 || {
+    echo "homebrew is not installed, installing ...";
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+}
+
+# install oh-my-zsh
+[[ -d $HOME/.oh-my-zsh/ ]] || {
+    echo "oh-my-zsh is not installed, installing ...";
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+}
