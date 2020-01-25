26b361c2881d5a8ec06a63bb05350d79804541a0
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2020-01-25 13:38:05 +0100

    spacemacs: do not use spacemacs|use-package-add-hook
    Somehow, this does not work for me. I have to load my config twice for
    it to work, which is an unwanted behaviour.
    
    replace spacemacs|use-package-add-hook by a regular (use-package) which
    works fine.
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- spacemacs-layers/makohoek-dev/packages.el
+++ spacemacs-layers/makohoek-dev/packages.el
@@ -58,30 +58,29 @@
 
 ;; magit is owned by layer 'git'
 (defun makohoek-dev/post-init-magit ()
-  (when (configuration-layer/package-used-p 'magit)
-    (spacemacs|use-package-add-hook magit
-      :post-config
-      ;; performance tricks for magit (useful in kernel tree)
-      ;; remove tag entry for magit status
-      (setq magit-status-headers-hook (remove 'magit-insert-tags-header magit-status-headers-hook))
-      ;; remove revision header in magit-diff
-      (setq magit-revision-sections-hook (remove 'magit-insert-revision-headers magit-revision-sections-hook))
-      ;; signed-off by default
-      ;; set signoff by default
-      (defvar transient-default-values '((magit-commit "--signoff")
-                                         (magit-revert "--signoff")
-                                         (magit-log:magit-log-mode "-n256" "--decorate")
-                                         (magit-rebase "--autosquash" "--autostash")))
-      (setq transient-values transient-default-values)
+  (use-package magit
+    :config
+    ;; performance tricks for magit (useful in kernel tree)
+    ;; remove tag entry for magit status
+    (setq magit-status-headers-hook (remove 'magit-insert-tags-header magit-status-headers-hook))
+    ;; remove revision header in magit-diff
+    (setq magit-revision-sections-hook (remove 'magit-insert-revision-headers magit-revision-sections-hook))
+    ;; signed-off by default
+    ;; set signoff by default
+    (defvar transient-default-values '((magit-commit "--signoff")
+                                       (magit-revert "--signoff")
+                                       (magit-log:magit-log-mode "-n256" "--decorate")
+                                       (magit-rebase "--autosquash" "--autostash")))
+    (setq transient-values transient-default-values)
 
-      ;; title must be no longer than 50
-      (setq git-commit-summary-max-length 50)
-      ;; line must not be longer than 72
-      (setq git-commit-fill-column 72)
-      ;; support Bug: + Change-Id: pseudo-headers
-      (add-to-list 'git-commit-known-pseudo-headers "Bug")
-      (add-to-list 'git-commit-known-pseudo-headers "Fixes")
-      (add-to-list 'git-commit-known-pseudo-headers "Change-Id"))))
+    ;; title must be no longer than 50
+    (setq git-commit-summary-max-length 50)
+    ;; line must not be longer than 72
+    (setq git-commit-fill-column 72)
+    ;; support Bug: + Change-Id: pseudo-headers
+    (add-to-list 'git-commit-known-pseudo-headers "Bug")
+    (add-to-list 'git-commit-known-pseudo-headers "Fixes")
+    (add-to-list 'git-commit-known-pseudo-headers "Change-Id")))
 
 ;; whitespace is owned by 'spacemacs-defaults' layer
 (defun makohoek-dev/post-init-whitespace ()
