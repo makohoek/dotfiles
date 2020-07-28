3590d6de70e49b4e3fd56dc372bd0a78387c666d
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2020-07-28 18:38:22 +0200

    spacemacs: makohoek-window: fix startup failure
    When editing the current popwin:special-display-config plist, we had an
    error since the *compilation* entry seemed to be removed.
    
    Use push() instead to add my own entry.
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- spacemacs/.spacemacs-layers/makohoek-window/packages.el
+++ spacemacs/.spacemacs-layers/makohoek-window/packages.el
@@ -19,20 +19,21 @@
 (defun makohoek-window/post-init-window-purpose ()
   (use-package window-purpose
     :config
-    ;; not really window-purpose related, but still window related
-    ;; never split vertically (one above another)
-    (setq split-height-threshold nil)
+    (progn
+      ;; not really window-purpose related, but still window related
+      ;; never split vertically (one above another)
+      (setq split-height-threshold nil)
 
-    ;; See: https://github.com/syl20bnr/spacemacs/issues/6649#issuecomment-234490739
-    ;; replace default entries
-    (setcdr (assoc "*compilation*" popwin:special-display-config)
-            '(:dedicated t :position right :stick t :noselect t :width 0.3))
+      ;; to edit popwin:special-display-config, see
+      ;; https://github.com/syl20bnr/spacemacs/issues/6649#issuecomment-234490739
+      (push '("*compilation*" :dedicated t :position right :stick t :noselect t :width 0.3)
+            popwin:special-display-config)
 
-    ;; add new entries
-    (push '("*ripgrep-search*" :dedicated t :position right :stick t :noselect nil :width 0.3)
-          popwin:special-display-config)
+      ;; add new entries
+      (push '("*ripgrep-search*" :dedicated t :position right :stick t :noselect nil :width 0.3)
+            popwin:special-display-config)
 
-    ;; sync popwin:special-display-config to pupo as documented
-    (pupo/update-purpose-config)))
+      ;; sync popwin:special-display-config to pupo as documented
+      (pupo/update-purpose-config))))
 
 ;;; packages.el ends here
