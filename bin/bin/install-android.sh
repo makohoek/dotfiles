346c71ea45d393ca81a9b1f12e2886d456285a72
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2020-12-04 15:22:23 +0100

    install-android: also adb remount before installing
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- bin/bin/install-android.sh
+++ bin/bin/install-android.sh
@@ -13,6 +13,8 @@ main() {
     local build_out=$(get_build_var PRODUCT_OUT)
     installed_files=$(eval jq \'.[\"$module_name\"]\' $build_out/module-info.json | jq -r -M '.installed | .[]')
 
+    adb root && adb remount && adb wait-for-device
+
     for f in $installed_files; do
         local_file=$f
         remote_file=${f#$build_out}
