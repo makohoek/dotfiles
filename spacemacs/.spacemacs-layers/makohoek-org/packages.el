3433d10301ddd53aa100e6def883e212220ea9d5
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2022-06-20 09:23:27 +0200

    spacemacs: org: use <year>.org as todo / pomodoro file
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- spacemacs/.spacemacs-layers/makohoek-org/packages.el
+++ spacemacs/.spacemacs-layers/makohoek-org/packages.el
@@ -68,7 +68,7 @@
     (setq org-capture-templates
       `(("t" "Todo" entry (file+headline org-default-notes-file "Tasks")
          "* TODO %?\n  %i\n  %a")
-        ("p" "Pomodoro" entry (file+olp+datetree "~/work/org/pomodoro-test.org" "Pomodoro")
+        ("p" "Pomodoro" entry (file+olp+datetree "~/work/org/pomodoro/2022.org" "Pomodoro")
          ,(concat "* stats [/][\%]\n:PROPERTIES:\n:LOGGING: nil\n:END:\n" (string-join (make-list 15 "\n** TODO ")))
          :tree-type week
          :jump-to-captured 't)))
