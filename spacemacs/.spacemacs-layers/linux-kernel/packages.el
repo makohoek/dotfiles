14fca560fb9f076ba730bf8ba56edaf77a44fa73
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2022-08-11 14:59:18 +0200

    spacemacs: layers: linux-kernel: remove some boilerplate comments
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- spacemacs/.spacemacs-layers/linux-kernel/packages.el
+++ spacemacs/.spacemacs-layers/linux-kernel/packages.el
@@ -22,22 +22,6 @@
 
 ;;; Commentary:
 
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `linux-kernel-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `linux-kernel/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `linux-kernel/pre-init-PACKAGE' and/or
-;;   `linux-kernel/post-init-PACKAGE' to customize the package as it is loaded.
-
 ;;; Code:
 
 (defconst linux-kernel-packages
