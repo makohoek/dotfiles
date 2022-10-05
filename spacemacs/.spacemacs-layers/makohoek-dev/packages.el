cdd24a100aad3a9dd007ea8ad42b0dd9bc5af9f3
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2022-10-05 10:40:11 +0200

    spacemacs: dev: remove some unused shell variables from exec-path
    I don't use those, so drop them.
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- spacemacs/.spacemacs-layers/makohoek-dev/packages.el
+++ spacemacs/.spacemacs-layers/makohoek-dev/packages.el
@@ -82,7 +82,7 @@
 (defun makohoek-dev/init-exec-path-from-shell ()
   (use-package exec-path-from-shell
     :config
-    (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "NIX_SSL_CERT_FILE" "NIX_PATH"))
+    (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO"))
       (add-to-list 'exec-path-from-shell-variables var))
     (exec-path-from-shell-initialize)))
 
