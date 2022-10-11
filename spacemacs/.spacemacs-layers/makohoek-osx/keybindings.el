6f13a14fb0697a67d32c45b1f9c3a992aaf088d6
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2022-10-11 13:46:31 +0200

    spacemacs: delete osx layer
    This has been unused for quite some time now, so get rid of it.
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- spacemacs/.spacemacs-layers/makohoek-osx/keybindings.el
+++
@@ -1,4 +0,0 @@
-;; system clipboard copy/paste feature
-(spacemacs/declare-prefix "o" "clipboard functions")
-(spacemacs/set-leader-keys "o y" 'copy-to-clipboard)
-(spacemacs/set-leader-keys "o p" 'paste-from-clipboard)
