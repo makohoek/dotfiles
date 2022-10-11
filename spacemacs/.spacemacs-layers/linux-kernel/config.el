da5f6064dca2d0fdf2f2c6ad93b0639416a05ae2
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2022-10-11 16:30:59 +0200

    spacemacs: layers: linux-kernel: add serial-term wrapper
    To save my favorite baudrates and use some modern completion read
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

---
+++ spacemacs/.spacemacs-layers/linux-kernel/config.el
@@ -0,0 +1,6 @@
+(defvar linux-kernel-serial-term-default-port "/dev/ttyUSB0"
+  "default serial-term port path")
+
+(defvar linux-kernel-serial-term-default-speeds
+  (list "115200" "921600" "1500000")
+  "list of default serial-term baud rates")
--- spacemacs/.spacemacs-layers/linux-kernel/funcs.el
+++ spacemacs/.spacemacs-layers/linux-kernel/funcs.el
@@ -1,3 +1,12 @@
 (defun linux-kernel/reviewed-by ()
   (interactive)
   (insert (concat "Reviewed-by: " user-full-name " <" user-mail-address ">")))
+
+(defun linux-kernel/serial-term ()
+  (interactive)
+  (let ((speed (completing-read "Speed (b/s) "
+                             linux-kernel-serial-term-default-speeds)))
+    (serial-term
+     linux-kernel-serial-term-default-port
+     (string-to-number speed))))
+
