05e66c117660335e6a2288254b35c69588da4f9a
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2024-10-29 09:19:57 +0100

    spacemacs: layers: linux-kernel: Add Tested-By shortcut
    Similar to Reviewed-By
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- spacemacs/.spacemacs-layers/linux-kernel/funcs.el
+++ spacemacs/.spacemacs-layers/linux-kernel/funcs.el
@@ -2,6 +2,10 @@
   (interactive)
   (insert (concat "Reviewed-by: " user-full-name " <" user-mail-address ">")))
 
+(defun linux-kernel/tested-by ()
+  (interactive)
+  (insert (concat "Tested-by: " user-full-name " <" user-mail-address ">")))
+
 (defun linux-kernel/serial-term ()
   (interactive)
   (let ((speed (completing-read "Speed (b/s) "
--- spacemacs/.spacemacs-layers/linux-kernel/keybindings.el
+++ spacemacs/.spacemacs-layers/linux-kernel/keybindings.el
@@ -2,3 +2,6 @@
 
 (spacemacs/set-leader-keys-for-major-mode 'notmuch-message-mode
   "pr" 'linux-kernel/reviewed-by)
+
+(spacemacs/set-leader-keys-for-major-mode 'notmuch-message-mode
+  "pt" 'linux-kernel/tested-by)
